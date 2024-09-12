ce4a00e0b50904620d8826972bbf394ce0d16262
Hashara13 <hnethmin13@gmail.com>
2024-09-12 21:21:03 +0530

    nav added

--- app/page.jsx
+++ app/page.jsx
@@ -4,14 +4,14 @@ import FirstScreen from '@components/FirstScreen';
 const HomePage = () => {
   return (
     <section className='w-full h-screen flex items-center mt-2 flex-col'>
-      <h1 className='text-4xl font-bold leading-[1.15] text-black sm:text-6xl text-center'>
+      <h1 className='text-4xl font-bold leading-[1.15] text-white-100 sm:text-6xl text-center'>
       Find roadmaps and keywords
         <br className='max-md:hidden mb-5'/>
         <span className='mt-3 bg-gradient-to-r from-purple-500 via-green-600 to-blue-500 bg-clip-text text-transparent text-center'>
-        That will help you
+        That will help you....
         </span>
-        <p className='desc mt-5 text-xl text-gray-600 sm:text-xl max-w-4xl text-center'>
-        Create, and share Keywords,Joggles & Roadmaps related to Software Development Technologies
+        <p className='desc mt-5 text-xl text-gray-400 sm:text-xl max-w-4xl text-center font-regular'>
+        Create, and share Keywords,Joggles & Roadmaps related to <br></br>Software Development Technologies
         </p>
       </h1>
       <FirstScreen/>
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,14 +1,104 @@
 "use client";
-import React from 'react'
-import { useState , useEffect} from 'react';
-import Link from 'next/link';
-import {signIn,signOut, useSession, getProviders} from 'next-auth/react'
-import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import Image from "next/image";
 
 const NavBar = () => {
-  return (
-    <nav>NavBar</nav>
-  )
-}
+  const isUserLoggedIn = true;
+  const[providers, setProviders]=useState(null);
 
-export default NavBar
+  useEffect(()=>{
+    const setProviders=async()=>{
+      const response=await getProviders();
+      setProviders(response);
+    }
+    setProviders();
+  },[])
+
+
+  return (
+    <nav className="flex w-full items-center mt-5 ml-2 mb-5">
+      <Link href="/" className="flex gap-2 items-center">
+        <Image
+          src="/assets/images/logo.png"
+          width={180}
+          height={180}
+          className="object-contain"
+        />
+      </Link>
+      <div className="ml-auto sm:flex hidden ">
+        {isUserLoggedIn ? (
+          <>
+            <Link
+              href="/add"
+              className="mr-4 rounded-full bg-gray-300 py-2 px-5 text-black transition-all hover:bg-gray-600 hover:text-white text-center text-m font-inter font-medium flex items-center"
+            >
+              Add New
+            </Link>
+            <button
+              type="button"
+              onClick={signOut}
+              className="mr-2 rounded-full border-gray-100 py-2 px-5 text-white transition-all hover:bg-red-900 hover:text-white text-center text-m font-inter font-medium flex items-center"
+            >
+              Log Out
+              </button>
+              <Link href="/userProfile">
+                <Image
+          src="/assets/images/logo-black.png"
+          alt="username"
+                  className="ml-2 rounded-full mr-5"
+                  width={35}
+                  height={35}
+                />
+              </Link>
+        
+          </>
+        ) : (
+          <>
+          {providers && Object.values(providers).map((provider)=>(
+             <button
+             type="button"
+             onClick={()=>signIn(provider.id)}
+             key={provider.name}
+             className="mr-2 rounded-full bg-green-100 py-2 px-5 text-white transition-all hover:bg-black-900 hover:text-white text-center text-m font-inter font-medium flex items-center"
+           >
+             Log Out
+             </button>
+          ))}
+          </>
+        )}
+      </div>
+
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+                 <Image
+         alt="username"
+         className="mr-20 rounded-full mr-5"
+         width={35}
+         height={35}
+          onClick={()=>{}}
+          src="/assets/images/logo-black.png"
+        />
+            </div>
+        ):(
+          <>
+          {providers && Object.values(providers).map((provider)=>(
+             <button
+             type="button"
+             onClick={()=>signIn(provider.id)}
+             key={provider.name}
+             className="mr-2 rounded-full bg-green-100 py-2 px-5 text-white transition-all hover:bg-black-900 hover:text-white text-center text-m font-inter font-medium flex items-center"
+           >
+             Log Out
+             </button>
+          ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
